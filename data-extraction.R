e8b02facfa89e94dd4d5ec1d3d9ea247ef0a6ebd
Rocco Barbini <roccobarbi@gmail.com>
2015-02-05 07:34:07 +0100

    Updated the code to draw the first graph
    Still to be perfected (frequency axis)

--- data-extraction.R
+++ data-extraction.R
@@ -1,4 +1,5 @@
-household_Data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+household_Data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
 household_Data[,"Date"] <- as.Date(household_Data[,"Date"], format = "%d/%m/%Y")
 household_Data <- household_Data[household_Data[,"Date"] == as.Date("2007-02-01") | household_Data[,"Date"] == as.Date("2007-02-02"),]
-household_Data[,"DateTime"]  <- as.POSIXct(paste(household_Data[,"Date"], household_Data[,"Time"]), format="%Y-%m-%d %H:%M:%S")
+household_Data$datetime  <- as.POSIXct(paste(household_Data$Date, household_Data$Time), format="%Y-%m-%d %H:%M:%S")
+hist(as.numeric(household_Data$Global_active_power), main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
