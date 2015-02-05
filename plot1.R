187ffccfef4322c5e4a2d544a93ba7020a64707c
Rocco Barbini <roccobarbi@gmail.com>
2015-02-05 22:30:01 +0100

    Improved plot1.R

--- plot1.R
+++ plot1.R
@@ -4,5 +4,5 @@ household_Data <- household_Data[which(household_Data$Date == as.Date("2007-02-0
 household_Data$datetime  <- as.POSIXct(paste(household_Data$Date, household_Data$Time), format="%Y-%m-%d %H:%M:%S")
 par("ps" = 13)
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
-hist(as.numeric(household_Data$Global_active_power), main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red", ylim = c(0,1200))
+hist(household_Data$Global_active_power, main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red", ylim = c(0,1200))
 dev.off()
