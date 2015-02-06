d2f6ebf303db60fe6e4ae93cf8284835d4749663
Rocco Barbini <roccobarbi@gmail.com>
2015-02-06 17:48:51 +0100

    Improved the language settings for plot4.R

--- plot4.R
+++ plot4.R
@@ -2,6 +2,9 @@ household_Data <- read.csv("household_power_consumption.txt", header = TRUE, sep
 household_Data[,"Date"] <- as.Date(household_Data[,"Date"], format = "%d/%m/%Y")
 household_Data <- household_Data[which(household_Data$Date == as.Date("2007-02-01") | household_Data$Date == as.Date("2007-02-02")),]
 household_Data$datetime  <- as.POSIXct(paste(household_Data$Date, household_Data$Time), format="%Y-%m-%d %H:%M:%S")
+#store langugage settings
+user_lang <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME", "English")
 png(filename = "plot4.png", width = 480, height = 480, bg = "transparent")
 par("ps" = 13, mfrow = c(2,2))
 # First Plot
@@ -20,3 +23,5 @@ legend("topright", lty = 1, col = c("black", "blue", "red"), legend = c("Sub_met
 plot(household_Data$datetime, household_Data$Global_reactive_power, type = "n", xlab="datetime", ylab="Global_reactive_power")
 lines(household_Data$datetime, household_Data$Global_reactive_power, type = "l")
 dev.off()
+#restore language
+Sys.setlocale("LC_TIME", user_lang)
